e0dfa7924e41255012fbe928a295791a02879a7e
Liz <lizzi8624@hotmail.com>
2024-12-05 20:51:53 +0000

    Finalised

--- src/index.js
+++ src/index.js
@@ -13,13 +13,13 @@ function generatePoem(event) {
   let instructionsInput = document.querySelector("#user-instructions");
   let apiKey = "93b084b6to4250fb8d980f6fba780ad2";
   let context =
-    "You are a romantic Poem expert and love to write short poems. You mission is to generate a 4 line poem in basic HTML and separate each line with a <br />. Make sure to follow the user instructions. Do not include a title to the poem. Sign the poem with 'Liz AI' inside a <strong> element at the end of the poem and NOT at the beginning. Don't use any markdown";
-  let prompt = `User instructions: Generate a washing line poem inclduing  ${instructionsInput.value}`;
+    "You are a creative Poem expert and love to write short poems. You mission is to generate a 4 line poem in basic HTML and separate each line with a <br />. Make sure to follow the user instructions. Do not include a title to the poem. Sign the poem with 'Liz AI' inside a <strong> element at the end of the poem and NOT at the beginning. Don't use any markdown";
+  let prompt = `User instructions: Generate a poem about dogd with the name ${instructionsInput.value}`;
   let apiURL = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
 
   let poemElement = document.querySelector("#poem");
   poemElement.classList.remove("hidden");
-  poemElement.innerHTML = `<div class="generating"> 🧺 collecting the pegs 🧺</div>`;
+  poemElement.innerHTML = `<div class="generating"> 🦴 fetiching 🦴  </div>`;
 
   axios.get(apiURL).then(displayPoem);
 }
