0f5ba91b36462c2ef0febcb5d66cec941b10d1c9
Liz <lizzi8624@hotmail.com>
2024-12-08 12:04:17 +0000

    initial commit

--- src/index.js
+++ src/index.js
@@ -13,7 +13,7 @@ function generateLyric(event) {
   let instructionsInput = document.querySelector("#user-instructions");
   let apiKey = "93b084b6to4250fb8d980f6fba780ad2";
   let context =
-    "You are a creative heavy metal lyric expert and love to write lyrics. You mission is to generate a death metal song in basic HTML and separate each line with a <br />. Make sure to follow the user instructions. Do not include a title to the lyrics. Sign the lyrics with 'Liz AI' inside a <strong> element at the end of the lyrics and NOT at the beginning. Don't use any markdown.";
+    "You are a creative heavy metal lyric expert and love to write lyrics. You mission is to generate a death metal song in basic HTML and separate each line with a <br />. Make sure to follow the user instructions. Do not include a title to the lyrics. The lyrics should be intense and dark. Not all the lines should rhyme. Sign the lyrics with 'Liz AI' inside a <strong> element at the end of the lyrics and NOT at the beginning. Don't use any markdown.";
   let prompt = `User instructions: Generate deathcore lyrics about ${instructionsInput.value}`;
   let apiURL = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
 
